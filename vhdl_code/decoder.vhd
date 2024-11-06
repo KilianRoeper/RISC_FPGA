f297579230fe991eb2bc082dd7a59fb44c9045ed
KilianRoeper <163199395+KilianRoeper@users.noreply.github.com>
2024-11-06 16:13:22 +0100

    Update decoder.vhd

--- vhdl_code/decoder.vhd
+++ vhdl_code/decoder.vhd
@@ -21,6 +21,11 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+library work;
+use work.RISC_constants.OPCODE_SW;
+use work.RISC_constants.OPCODE_BEQ;
+use work.RISC_constants.OPCODE_B;
+
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,16 +37,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity decoder is
-Port (clk_in : in  STD_LOGIC;
-      enable_in : in  STD_LOGIC; 
-      instruction_in : in STD_LOGIC_VECTOR (15 downto 0);
-      regA_select_out : out STD_LOGIC_VECTOR (2 downto 0);
-      regB_select_out : out STD_LOGIC_VECTOR (2 downto 0);
-      regC_select_out : out STD_LOGIC_VECTOR (2 downto 0);
-      im_data_out : out STD_LOGIC_VECTOR (15 downto 0);
-      alu_op_out : out STD_LOGIC_VECTOR (4 downto 0);
-      regA_write_out : out  STD_LOGIC;
-      store_enable_out : out  STD_LOGIC
+Port (  clk_in            : in STD_LOGIC;
+        enable_in         : in STD_LOGIC; 
+        instruction_in    : in STD_LOGIC_VECTOR (15 downto 0);
+        
+        store_enable_out  : out STD_LOGIC;
+        alu_op_out        : out STD_LOGIC_VECTOR (4 downto 0);
+        im_data_out       : out STD_LOGIC_VECTOR (15 downto 0);
+        regA_write_out    : out STD_LOGIC;
+        regA_select_out   : out STD_LOGIC_VECTOR (2 downto 0);
+        regB_select_out   : out STD_LOGIC_VECTOR (2 downto 0);
+        regC_select_out   : out STD_LOGIC_VECTOR (2 downto 0)  
       );
 end decoder;
 
@@ -58,15 +64,18 @@ begin
         alu_op_out <= instruction_in(15 downto 12) & instruction_in(8);
         
         case instruction_in(15 downto 12) is
-        when "0101" => 	-- SW
+        when OPCODE_SW =>
           regA_write_out <= '0';
           store_enable_out <= '1';
-        when "0110" => 	-- BEQ
+           
+        when OPCODE_BEQ => 	
           regA_write_out <= '0';
           store_enable_out <= '0';
-        when "0111" => 	-- B
+          
+        when OPCODE_B => 	
           regA_write_out <= '0';
           store_enable_out <= '0';
+          
         when others =>
           regA_write_out <= '1';
           store_enable_out <= '0';
