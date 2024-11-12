31cf8ad08fcc9483392d5d7a5bde2590edaa6b52
KilianRoeper <163199395+KilianRoeper@users.noreply.github.com>
2024-11-12 12:18:52 +0100

    Update PC.vhd

--- vhdl_code/PC.vhd
+++ vhdl_code/PC.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04.11.2024 22:39:19
--- Design Name: 
--- Module Name: PC - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 library work;
@@ -33,32 +12,35 @@ use IEEE.NUMERIC_STD.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity PC is
+entity pc is
 Port (  clk_in   : in STD_LOGIC;
         pc_op_in : in STD_LOGIC_VECTOR (1 downto 0);
         pc_in    : in STD_LOGIC_VECTOR (15 downto 0);
+        branch_in   : in STD_LOGIC;
         
         pc_out   : out STD_LOGIC_VECTOR (15 downto 0) 
     );
 end PC;
 
 architecture Behavioral of PC is
-  signal current_pc: std_logic_vector( 15 downto 0) := X"0000";
+  signal current_pc: STD_LOGIC_VECTOR(15 downto 0) := X"0000";
 begin
 
   process (clk_in)
   begin
     if rising_edge(clk_in) then
-      case pc_op_in is
-        when PC_OP_NOP => 	         -- NOP, keep PC the same/halt
-        when PC_OP_INC => 	         -- increment
-          current_pc <= std_logic_vector(unsigned(current_pc) + 1);
-        when PC_OP_ASSIGN => 	     -- set from external input
-          current_pc <= pc_in;        
-        when PC_OP_RESET => 	     -- Reset
-          current_pc <= X"0000";
-        when others =>
-      end case;
+        if branch_in = '1' then 
+            current_pc <= pc_in;
+        else
+          case pc_op_in is
+            when PC_OP_NOP => 	         -- 00 / halt
+            when PC_OP_INC => 	         -- 01 / increment
+              current_pc <= STD_LOGIC_VECTOR(unsigned(current_pc) + 1);       
+            when PC_OP_RESET => 	     -- 11 / reset
+              current_pc <= X"0000";
+            when others =>
+          end case;
+        end if;
     end if;
   end process;
 
