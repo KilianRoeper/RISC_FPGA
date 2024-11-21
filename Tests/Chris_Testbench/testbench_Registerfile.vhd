a1be18dc128d872d205dbf10838353b2b8d67728
unknown <chris.mueller7@web.de>
2024-11-21 13:01:27 +0100

    update testbench: register_file, code decoder

--- Tests/Chris_Testbench/testbench_Registerfile.vhd
+++ Tests/Chris_Testbench/testbench_Registerfile.vhd
@@ -9,7 +9,7 @@ end simulation_decoder;
 architecture Behavioral of simulation_decoder is
   constant clk_time :time    := 5 ns;
   constant waitTime :time    := 2 * clk_time;
-  constant InvalidTime :time := 1.5 * clk_time;
+  
   
   signal test_clk_in          : STD_LOGIC := '1';
   signal test_enable_in       : STD_LOGIC := '0';
@@ -46,52 +46,144 @@ begin
 
   Testing_register: process
   begin
+  --Activation
         test_enable_in <= '1';
-        test_regB_select_in <= "000";
-        test_regC_select_in <= "111";
-        wait for waitTime;
-        test_regB_select_in <= "001";
-        test_regC_select_in <= "110";
         test_write_enable_in <= '1';
+  -- Write 000 => 1010 1010 1010 1010 AAAA
+  -- Read  000
         test_regA_data_in <= "1010101010101010";
-        wait for waitTime;
-        test_regB_select_in <= "010";
-        test_regC_select_in <= "101";
-        test_write_enable_in <= '0';
-        test_regA_data_in <= "0101010101010101";
-        wait for waitTime;
-        test_regB_select_in <= "011";
-        test_regC_select_in <= "100";
-        test_write_enable_in <= '1';
-        test_regA_data_in <= "0101010101010101";
-        wait for waitTime;
-        test_regB_select_in <= "100";
-        test_regC_select_in <= "011";
-        test_write_enable_in <= '0';
-        test_regA_data_in <= "0000111100001111";
-        wait for waitTime;
-        test_regB_select_in <= "101";
-        test_regC_select_in <= "010";
-        test_write_enable_in <= '1';
-        test_regA_data_in <= "0000111100001111";
-        wait for waitTime;
-        test_regB_select_in <= "110";
-        test_regC_select_in <= "001";
-        test_write_enable_in <= '1';
-        test_regA_data_in <= "1111111111111111";
-        wait for waitTime;
-        test_regB_select_in <= "111";
-        test_regC_select_in <= "000";
-        test_write_enable_in <= '1';
-        test_regA_data_in <= "0000000000000000";
-        wait for waitTime;
+        test_regA_select_in <= "000";
         test_regB_select_in <= "000";
         test_regC_select_in <= "000";
+        wait for clk_time;
+ 
         wait for waitTime;
-        test_enable_in <= '1';
+        
+        
+         -- Write 001 => 0101 0101 0101 0101 5555
+         -- Read  000
+        test_regA_data_in <= "0101010101010101";
+        test_regA_select_in <= "001";
+        test_regB_select_in <= "000";
+        test_regC_select_in <= "000";
+      
+        wait for waitTime;
+        
+         -- Write 010 => 0000 1111 0000 1111 0F0F
+         -- Read  001
+        test_regA_data_in <= "0000111100001111";
+        test_regA_select_in <= "010";
+        test_regB_select_in <= "001";
+        test_regC_select_in <= "001";
+        wait for waitTime;
+       
+       
+        
+         -- Write 011 => 1111 0000 1111 0000 F0F0
+         -- Read  010
+        test_regA_data_in <= "1111000011110000";
+        test_regA_select_in <= "011";
+        test_regB_select_in <= "010";
+        test_regC_select_in <= "010";
+        wait for waitTime;
+        
+        -- Write 100 => 0011 0011 0011 0011 3333
+        -- Read  011
+        test_regA_data_in <= "0011001100110011";
+        test_regA_select_in <= "100";
+        test_regB_select_in <= "011";
+        test_regC_select_in <= "011";
+        wait for clk_time;
+      
+        wait for clk_time;
+        
+        -- Write 101 => 0011 1100 1111 0011 3CF3
+        -- Read  100
+        test_regA_data_in <= "0011110011110011";
+        test_regA_select_in <= "101";
+        test_regB_select_in <= "100";
+        test_regC_select_in <= "100";
+        wait for waitTime;
+        
+        
+        -- Write 110 => 1100 1111 0011 1100 CF3C
+        -- Read  101        
+        test_regA_data_in <= "1100111100111100";
+        test_regA_select_in <= "110";
+        test_regB_select_in <= "101";
+        test_regC_select_in <= "101";
+        wait for waitTime;
+        
+        
+        -- Write 111 => 0011 1010 1001 0101 3A95
+        -- Read  110
+        test_regA_data_in <= "0011101010010101";
+        test_regA_select_in <= "111";
+        test_regB_select_in <= "110";
+        test_regC_select_in <= "110";
+        wait for waitTime;
+        
+        
+        
+        -- Write 000 => 1110 0100 1001 1110 E49E
+        -- Read  111
+        test_regA_data_in <= "1110010010011110";
+        test_regA_select_in <= "000";
         test_regB_select_in <= "111";
         test_regC_select_in <= "111";
+        wait for waitTime;
+        
+        
+        -- Write 010 => 1110 0100 1001 1110 E49E
+        -- Read  000 and 111
+        test_regA_data_in <= "1110010010011110";
+        test_regA_select_in <= "010";
+        test_regB_select_in <= "000";
+        test_regC_select_in <= "111";
+       wait for waitTime;
+        
+        
+        --Disable write
+        test_write_enable_in <= '0';
+       
+        -- Write 011 => 1110 0100 1001 1110 E49E
+        -- Read  000 and 010
+        test_regA_data_in <= "1110010010011110";
+        test_regA_select_in <= "011";
+        test_regB_select_in <= "010";
+        test_regC_select_in <= "010";
+        wait for waitTime;
+        
+        
+        
+        -- Read  000 and 010
+        test_regB_select_in <= "011";
+        test_regC_select_in <= "011";
+        wait for clk_time; 
+        --Profment
+--        assert test_regB_out = "1110010010011110" report "regB_out error" severity error;
+--        assert test_regC_out = "1110010010011110" report "regC_out error" severity error;
+        wait for clk_time; 
+        
+        -- Disable in, enable Write 
+        test_enable_in <= '0';
         test_write_enable_in <= '1';
-        test_regA_data_in <= "1111111111111111";
+        
+        -- Write 011 => 1110 1110 1110 1110 EEEE
+        -- Read  000 and 010
+        test_regA_data_in <= "1110111011101110";
+        test_regA_select_in <= "011";
+        test_regB_select_in <= "011";
+        test_regC_select_in <= "011";
+        wait for waitTime;
+        
+        -- Read  010  
+        test_regB_select_in <= "011";
+        test_regC_select_in <= "011";
+        wait for clk_time;
+        --Profment
+--        assert test_regB_out = "1110010010011110" report "regB_out error" severity error;
+--        assert test_regC_out = "1110010010011110" report "regC_out error" severity error;
+        wait for clk_time; 
   end process; 
 end Behavioral;
