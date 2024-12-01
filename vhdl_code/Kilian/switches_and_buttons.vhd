d9d0a09c90d014b76fd22ad8c37269911a67b864
KilianRoeper <163199395+KilianRoeper@users.noreply.github.com>
2024-12-01 13:51:03 +0100

    Update switches_and_buttons.vhd

--- vhdl_code/Kilian/switches_and_buttons.vhd
+++ vhdl_code/Kilian/switches_and_buttons.vhd
@@ -21,6 +21,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+library work;
+use work.risc_constants.DEBOUNCE_THRESHOLD;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,33 +34,39 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity switches_and_buttons is
---  Port ( );
+  Port ( 
+        clk_in      : in STD_LOGIC;
+        sw_in       : in STD_LOGIC;
+        deb_sw_out  : out STD_LOGIC
+        );
 end switches_and_buttons;
 
 architecture Behavioral of switches_and_buttons is
     -- switches 
-    --signal debounced_switch0    : STD_LOGIC := '0';
+    signal debounced_switch0    : STD_LOGIC := '0';
     
-    -- CPU signals/ constants
-    --signal cpu_clock_and_switch : STD_LOGIC := '0';
-begin
-    -- anding switch with clock for further control
---    cpu_clock_and_switch <= debounced_switch0 and cpu_clock;
+begin    
+    debounce: process(clk_in)
+    variable debounce_counter: integer := 0;
+    begin
+        if rising_edge(clk_in) then
+            if sw_in /= debounced_switch0 then
+                debounce_counter := debounce_counter + 1;
+                if debounce_counter > DEBOUNCE_THRESHOLD then
+                    debounced_switch0 <= sw_in;
+                    debounce_counter := 0;
+                end if;
+            else
+                debounce_counter := 0;
+            end if;
+        end if;
+    end process;
     
---    switch_debounce: process(cpu_clock)
---    variable debounce_counter: integer := 0;
---    begin
---        if rising_edge(cpu_clock) then
---            if sw0 /= debounced_switch0 then
---                debounce_counter := debounce_counter + 1;
---                if debounce_counter > DEBOUNCE_THRESHOLD then
---                    debounced_switch0 <= sw0;
---                    debounce_counter := 0;
---                end if;
---            else
---                debounce_counter := 0;
---            end if;
---        end if;
---    end process;
+    set_switch : process(clk_in, debounced_switch0)
+    begin   
+        if rising_edge(clk_in) then
+            deb_sw_out <= debounced_switch0;
+        end if;
+    end process;
 
 end Behavioral;
