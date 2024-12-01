913e14fda6753e487f2149290896073c4ffa89ac
KilianRoeper <163199395+KilianRoeper@users.noreply.github.com>
2024-12-01 20:38:02 +0100

    Update pc.vhd

--- vhdl_code/teamcode/code/pc.vhd
+++ vhdl_code/teamcode/code/pc.vhd
@@ -1,16 +1,15 @@
+----------------------------------------------------------------------------------
+-- Entity: program counter 
+-- Name: Kelly Velten
+----------------------------------------------------------------------------------
+
+-- handles the program counter based on the given opcode and the branch signal 
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 library work;
 use work.risc_constants.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity pc is
 Port (  clk_in      : in STD_LOGIC;
@@ -29,14 +28,15 @@ begin
   process (clk_in)
   begin
     if rising_edge(clk_in) then
+        -- branch to the address given by pc_in if the branch_in signal is set high
         if branch_in = '1' then 
             current_pc <= pc_in;
         else
           case pc_op_in is
-            when PC_OP_NOP => 	         -- 00 / halt
-            when PC_OP_INC => 	         -- 01 / increment
+            when PC_OP_NOP => 	                                             -- 00 / halt
+            when PC_OP_INC => 	                                             -- 01 / increment
               current_pc <= STD_LOGIC_VECTOR(unsigned(current_pc) + 1);       
-            when PC_OP_RESET => 	     -- 11 / reset
+            when PC_OP_RESET => 	                                     -- 11 / reset
               current_pc <= X"0000";
             when others =>
           end case;
